167067d5fda9e558e9cfdd95e9d63e11a9f65419
Szymon Witamborski <santamon@gmail.com>
2010-03-26 20:43:50 +0100

    Deleted what supposed to be in sw.gui

--- neuronet.clj
+++ neuronet.clj
@@ -22,20 +22,4 @@
 			JSpinner
 			SpinnerNumberModel)
 	   (javax.swing.text JTextComponent)))
-()
 
-;; proste makro zwracajace funkcje ze stringa
-(defmacro callOn [s]
-  `(fn [x#] (~(symbol s) x#)))
-
-
-(defmacro method-str [s]
-  `(fn [obj# param#] (~(symbol s) obj# param#) obj#))
-(defn method-str2 [s]
-  (fn [obj param] ((resolve (symbol s))
-		   obj param)))
-(defn setter [p]
-  (let [cs (rest (seq (str p)))
-	fletter (Character/toUpperCase (first cs))
-	s (reduce str  (list ".set" fletter (reduce str (rest cs))))]
-    (method-str s)))
